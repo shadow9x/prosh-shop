0abd973341118da1b453c9bd1cd250fbda918d3e
thjkjnoidi <thjkinoidi2@gmail.com>
2020-10-28 19:07:01 +0700

    Rever replace & with vnd

--- frontend/src/components/Product.js
+++ frontend/src/components/Product.js
@@ -24,7 +24,7 @@ const Product = ({ product }) => {
           />
         </Card.Text>
 
-        <Card.Text as='h3'>{product.price}₫</Card.Text>
+        <Card.Text as='h3'>${product.price}</Card.Text>
       </Card.Body>
     </Card>
   )
--- frontend/src/components/ProductCarousel.js
+++ frontend/src/components/ProductCarousel.js
@@ -28,7 +28,7 @@ const ProductCarousel = () => {
             <Image src={product.image} alt={product.name} fluid />
             <Carousel.Caption className='carousel-caption'>
               <h2>
-                {product.name} ({product.price}₫)
+                ${product.name} ({product.price})
               </h2>
             </Carousel.Caption>
           </Link>
