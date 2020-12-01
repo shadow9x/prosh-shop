74646b06ca870c7c82149a09095054c1b1101e25
thjkjnoidi <thjkinoidi2@gmail.com>
2020-12-01 21:31:02 +0700

    Update google analytics

--- frontend/src/components/Meta.js
+++ frontend/src/components/Meta.js
@@ -8,6 +8,14 @@ const Meta = ({ title, description, keywords, image }) => {
       <meta name='description' content={description} />
       <meta name='keyword' content={keywords} />
       <meta name="og:title" content={title} />
+      <script async src="https://www.googletagmanager.com/gtag/js?id=G-6GT4T07P4W"></script>
+      <script>{`
+        window.dataLayer = window.dataLayer || [];
+        function gtag(){dataLayer.push(arguments);}
+        gtag('js', new Date());
+
+        gtag('config', 'G-6GT4T07P4W');
+      `}</script>     
       { image && <meta name="og:image" content={image} /> }
     </Helmet>
   )
